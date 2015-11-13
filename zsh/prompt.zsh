38a845bf3e2cd07afc4af3ad2a92d52545ed6450
Bobby Ullman <robert.j.ullman@gmail.com>
2015-11-12 20:28:05 -0800

    make zsh prompt faster for large git repos

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,1 +1,6 @@
 DEFAULT_USER="bullman"
+
+function git_prompt_info() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+}
