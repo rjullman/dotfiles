041cd375c45215a98c4ff059560998b20076e639
Bobby Ullman <bullman@palantir.com>
2015-11-21 10:39:03 -0800

    add right prompt with time

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,4 @@
 DEFAULT_USER="bullman"
 
-function git_prompt_info() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
-}
+# add right prompt to show time
+RPROMPT="%{$fg[yellow]%}[%*]"
