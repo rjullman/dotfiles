918963ba483fc2a405a762f67fcbb35afd0e069b
Bobby Ullman <bullman@palantir.com>
2016-10-17 14:32:23 -0700

    use version independent path and manpath setup

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,3 @@
 # setup path and man path
-local BREW_PREFIX=`brew --prefix`
-export PATH="$BREW_PREFIX/bin:$BREW_PREFIX/Cellar/coreutils/8.24/libexec/gnubin:$ZSH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
-export MANPATH="$BREW_PREFIX/Cellar/coreutils/8.24/libexec/gnuman:${MANPATH-/usr/share/man}"
+export PATH="$(brew --prefix)/bin:$(brew --prefix coreutils)/libexec/gnubin:$ZSH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+export MANPATH="$(brew --prefix coreutils)/libexec/gnuman:${MANPATH-/usr/share/man}"
