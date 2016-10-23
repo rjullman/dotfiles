2e81484d67f0c627cd82c48c336aa39106704776
Bobby Ullman <robert.j.ullman@gmail.com>
2016-10-22 23:29:06 -0700

    change ZSH to more descriptive DOTFILES

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
 # setup path and man path
-export PATH="$(brew --prefix)/bin:$(brew --prefix coreutils)/libexec/gnubin:$ZSH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+export PATH="$(brew --prefix)/bin:$(brew --prefix coreutils)/libexec/gnubin:$DOTFILES:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 export MANPATH="$(brew --prefix coreutils)/libexec/gnuman:${MANPATH-/usr/share/man}"
