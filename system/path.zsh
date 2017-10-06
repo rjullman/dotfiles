de8371323f8d0fc896aad1ba7a5f72a30845a2d6
Bobby Ullman <robert.j.ullman@gmail.com>
2017-10-06 09:19:16 -0700

    speed up terminal startup
    by not using `brew --prefix <formula>` (which is slow)
    
    note that `brew --prefix` is fast

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
 # setup path and man path
-export PATH="$(brew --prefix)/bin:$(brew --prefix coreutils)/libexec/gnubin:$DOTFILES:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
-export MANPATH="$(brew --prefix coreutils)/libexec/gnuman:${MANPATH-/usr/share/man}"
+BREW_PREFIX="$(brew --prefix)"
+BREW_PREFIX_COREUTILS="$BREW_PREFIX/opt/coreutils" # `brew --prefix coreutils` is too slow
+export PATH="$BREW_PREFIX/bin:$BREW_PREFIX_COREUTILS/libexec/gnubin:$DOTFILES:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+export MANPATH="$BREW_PREFIX_COREUTILS/libexec/gnuman:${MANPATH-/usr/share/man}"
